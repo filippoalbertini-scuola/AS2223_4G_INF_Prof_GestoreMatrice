00a0afa7327a2f23640fdb716cc9cd9480f195da
Filippo Albertini <filippo@filippoalbertini.it>
2022-10-24 13:39:05 +0200

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_Prof_GestoreMatrice/GestoreMatrice.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_Prof_GestoreMatrice
+{
+    /// <summary>
+    /// Gestore di una matrice quadrata
+    /// </summary>
+    public class GestoreMatrice
+    {
+        // definizione e istanziazione matrice in base alle dimensioni indicate dall'utente
+        int[,] mat;
+
+        // dimensione della matrice
+        int matSize;
+
+        /// <summary>
+        /// Istanzia una matrice quadra di dimensione lato
+        /// </summary>
+        /// <param name="lato"></param>
+        public GestoreMatrice(int lato)
+        { 
+        }
+
+        /// <summary>
+        /// Istanzia e carica la matrice con la matrice passata come parametro
+        /// </summary>
+        /// <param name="mat"></param>
+        public GestoreMatrice(int[,] mat)
+        {
+        }
+
+        /// <summary>
+        /// Carica la matrice con valori casuali fra minValue e MaxValue
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        public void CaricaMatrice(int minValue, int maxValue)
+        { 
+        }
+
+        /// <summary>
+        /// Stampa la matrice nella console opportunamente formattata
+        /// </summary>
+        public void StampaMatrice()
+        { 
+        }
+
+        /// <summary>
+        /// Stampa la cornice della matrice da [0,0] per righe distinte in senso antiorario
+        /// </summary>
+        public void StampaCornice()
+        {
+        }
+
+        /// <summary>
+        /// Inverte i valori della prima colonna della matrice con l'ultima
+        /// </summary>
+        /// <returns></returns>
+        public void InvertiPrimaUltimaColonna()
+        {
+        }
+
+        /// <summary>
+        /// Cerca il valore passato nella matrice e restituisce il numero di occorrenze
+        /// </summary>
+        /// <param name="valoreDaCercare"></param>
+        /// <returns></returns>
+        public int ContaValore(int valoreDaCercare)
+        {
+            int nValori = 0;
+
+            return nValori;
+        }
+
+        /// <summary>
+        /// Calcola il valore minimo, massimo e medio (ritornato) dei valori inseriti nella matrice
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        public double CalcolaMinMaxMedio(ref int min, ref int max)
+        {
+            double medio = 0;
+
+
+            return medio;
+        }
+
+        /// <summary>
+        /// Ritorna il valore della matrice in una cella
+        /// </summary>
+        /// <param name="riga"></param>
+        /// <param name="colonna"></param>
+        /// <returns></returns>
+        public int GetValue(int riga, int colonna)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ AS2223_4G_INF_Prof_GestoreMatrice/Program.cs
@@ -0,0 +1,52 @@
+﻿// Scrivere un programma che :
+
+// 1) chiede in ingresso all’utente la dimensione di una matrice quadrata di numeri interi e la riempie con numeri casuali da -100 a 75 (punti 1)
+
+// 2) stampa formattata della matrice
+// 3) funzione di conteggio dei valori che restituisca il numero di occorrenze nella matrice del valore passato come parametro (punti 1)
+// 4) stampa i valori presenti nella cornice della matrice in senso "anti-"orario
+// 5) scrivere una funzione che inverte i valori della prima colonna della matrice con l'ultima
+// 6) scrivere un metodo che restituisce il valore medio, minimo e massimo dei valori contenuti nella matrice
+
+
+// dichiaro e istanzio l'oggetto per la gestione della matrice
+using AS2223_4G_INF_Prof_GestoreMatrice;
+
+const int MIN_VALUE_MAT = -100;
+const int MAX_VALUE_MAT = 75;
+
+// Main()
+
+GestoreMatrice m = new GestoreMatrice(10);
+
+m.StampaMatrice();
+
+// popola la matrice con valori causali nell'intervallo definito in firma
+m.CaricaMatrice(MIN_VALUE_MAT, MAX_VALUE_MAT);
+
+m.StampaMatrice();
+
+// stampa la cornice della matrice in senso "anti"-orario a partire da [0,0]
+m.StampaCornice();
+
+// visualizza la matrice
+m.StampaMatrice();
+
+// inverte i valori della prima colonna della matrice con l'ultima
+m.InvertiPrimaUltimaColonna();
+
+// visualizza la matrice
+m.StampaMatrice();
+
+// conta valori nella matrice
+int valoreDaCercare = 12;
+int nValori = m.ContaValore(12);
+Console.WriteLine("Trovati " + nValori + " occorrenze del valore " + valoreDaCercare);
+
+int min = 0, max = 0;
+double media = 0;
+
+// calcola il valore medio (ritornato) e il min e max della matrice
+media = m.CalcolaMinMaxMedio(ref min, ref max);
+
+Console.WriteLine("Min " + min + " max " + max + " e media dei valori " + media);
