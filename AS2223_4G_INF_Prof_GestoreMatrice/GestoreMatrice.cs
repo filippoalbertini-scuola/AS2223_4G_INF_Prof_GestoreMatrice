d1e8a4ca52b0f59e46c254bdfe16dfc615ed206a
Filippo Albertini <filippo@filippoalbertini.it>
2022-10-25 12:06:46 +0200

    Impostazione metodi della classe come da specifiche

--- AS2223_4G_INF_Prof_GestoreMatrice/GestoreMatrice.cs
+++ AS2223_4G_INF_Prof_GestoreMatrice/GestoreMatrice.cs
@@ -22,7 +22,12 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// </summary>
         /// <param name="lato"></param>
         public GestoreMatrice(int lato)
-        { 
+        {
+            // istanzia la matrice secondo le dimensioni dell'utente
+            mat = new int[lato, lato];
+
+            // mi salvo la larghezza della matrice
+            matSize = lato;
         }
 
         /// <summary>
@@ -31,6 +36,11 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// <param name="mat"></param>
         public GestoreMatrice(int[,] mat)
         {
+            // istanzia la matrice secondo le dimensioni dell'utente
+            this.mat = mat;
+
+            // mi salvo la larghezza della matrice
+            matSize = mat.GetUpperBound(0) + 1;
         }
 
         /// <summary>
@@ -39,14 +49,36 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// <param name="minValue"></param>
         /// <param name="maxValue"></param>
         public void CaricaMatrice(int minValue, int maxValue)
-        { 
+        {
+            // dichiara e istanzia il generatore di numeri casuali
+            Random rnd = new Random();
+
+            // scorri tutti gli elementi della matrice
+            for (int r = 0; r < matSize; r++)
+            {
+                for (int c = 0; c < matSize; c++)
+                {
+                    mat[r, c] = rnd.Next(minValue, maxValue + 1);
+                }
+            }
         }
 
         /// <summary>
         /// Stampa la matrice nella console opportunamente formattata
         /// </summary>
         public void StampaMatrice()
-        { 
+        {
+            // scorri tutti gli elementi della matrice
+            for (int r = 0; r < matSize; r++)
+            {
+                for (int c = 0; c < matSize; c++)
+                {
+                    Console.Write(mat[r, c].ToString().PadLeft(4) + " ");
+                }
+
+                // vai a capo a fine riga
+                Console.WriteLine();
+            }
         }
 
         /// <summary>
@@ -54,6 +86,21 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// </summary>
         public void StampaCornice()
         {
+            Console.WriteLine("lato sx");
+            for (int i = 0; i < matSize; i++)
+                Console.WriteLine(mat[i, 0]);
+
+            Console.WriteLine("lato inferiore senza il vertice in basso a sx");
+            for (int i = 1; i < matSize; i++)
+                Console.WriteLine(mat[matSize - 1, i]);
+
+            Console.WriteLine("lato dx senza il vertice in basso a dx");
+            for (int i = (matSize - 1) - 1; i >= 0; i--)
+                Console.WriteLine(mat[i, matSize - 1]);
+
+            Console.WriteLine("lato superiore senza vertice in alto a dx e a sx");
+            for (int i = (matSize - 1) - 1; i >= 1; i--)
+                Console.WriteLine(mat[0, i]);
         }
 
         /// <summary>
@@ -62,6 +109,15 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// <returns></returns>
         public void InvertiPrimaUltimaColonna()
         {
+            int temp;
+
+            // scorri tutti gli elementi della matrice
+            for (int r = 0; r < matSize; r++)
+            {
+                temp = mat[r, 0];
+                mat[r, 0] = mat[r, matSize - 1];
+                mat[r, matSize - 1] = temp;
+            }
         }
 
         /// <summary>
@@ -73,6 +129,16 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         {
             int nValori = 0;
 
+            // scorri tutti gli elementi della matrice
+            for (int r = 0; r < matSize; r++)
+            {
+                for (int c = 0; c < matSize; c++)
+                {
+                    if (mat[r, c] == valoreDaCercare)
+                        nValori += 1;
+                }
+
+            }
             return nValori;
         }
 
@@ -83,8 +149,29 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// <param name="max"></param>
         public double CalcolaMinMaxMedio(ref int min, ref int max)
         {
-            double medio = 0;
+            int mat_size = mat.GetUpperBound(0) + 1;
+            double medio;
 
+            min = mat[0, 0];
+            max = mat[0, 0];
+
+            medio = 0;
+            // scorri tutti gli elementi della matrice
+            for (int r = 0; r < mat_size; r++)
+            {
+                for (int c = 0; c < mat_size; c++)
+                {
+                    medio += mat[r, c];
+
+                    if (mat[r, c] > max)
+                        max = mat[r, c];
+
+                    if (mat[r, c] < min)
+                        min = mat[r, c];
+                }
+            }
+
+            medio = medio / (mat_size * mat_size);
 
             return medio;
         }
@@ -97,7 +184,7 @@ namespace AS2223_4G_INF_Prof_GestoreMatrice
         /// <returns></returns>
         public int GetValue(int riga, int colonna)
         {
-            return 0;
+            return mat[riga, colonna];
         }
 
     }
